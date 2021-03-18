4017243da1cfd321550b23a2478964650538ee7b
Midas <goncalomidoes@tecnico.ulisboa.pt>
2021-03-18 09:00:37 +0000

    Added text to see number of clicks

--- games/reactiontime/script.js
+++ games/reactiontime/script.js
@@ -1,70 +1,75 @@
-const logoNeec = document.getElementById('logo-neec');
-const printTime = document.getElementById('reaction-time');
-const startButton = document.getElementById('start-button');
-const countdown = document.getElementById('countdown');
-const clickBox = document.getElementById('click-box');
+const logoNeec = document.getElementById("logo-neec");
+const printTime = document.getElementById("reaction-time");
+const startButton = document.getElementById("start-button");
+const countdown = document.getElementById("countdown");
+const clickBox = document.getElementById("click-box");
+const total = document.getElementById("total-clickes");
 
 var startTime = 0;
 var clickedTime = 0;
 var reactionTime = 0;
 var gameStart = false;
+var total_clicks = 0;
 
 /**
  * creates a box with random coordinates and records its creation time
  */
 
-function makeBox() { 
-    var time = Math.random();
-    time = (time * 3000) + 500;
+function makeBox() {
+  var time = Math.random();
+  time = time * 3000 + 500;
 
-    setTimeout( () => {
-        var top = Math.random();
-        var left = Math.random() - 0.5;
-        top *= 20;
-        left *= 30;
+  setTimeout(() => {
+    var top = Math.random();
+    var left = Math.random() - 0.5;
+    top *= 20;
+    left *= 30;
 
-        logoNeec.style.top = top + 'vh';
-        logoNeec.style.left = left + 'vw';
-        
-        startTime=Date.now();
+    logoNeec.style.top = top + "vh";
+    logoNeec.style.left = left + "vw";
 
-        logoNeec.style.display = 'block'
+    startTime = Date.now();
 
-    }, time);
+    logoNeec.style.display = "block";
+  }, time);
 }
 
 clickBox.onclick = () => {
-    if (startTime === 0 && gameStart) {
-        alert('Espera até a imagem aparecer para clicares!');
-    }
-}
+  if (startTime === 0 && gameStart) {
+    alert("Espera até a imagem aparecer para clicares!");
+    total_clicks++;
+    total.innerHTML = `Clicks fora do tempo: ${total_clicks}`;
+  }
+};
 
 logoNeec.onclick = () => {
-    clickedTime = Date.now();
-    reactionTime = (clickedTime - startTime) / 1000;
+  clickedTime = Date.now();
+  reactionTime = (clickedTime - startTime) / 1000;
 
-    printTime.innerHTML = 'O teu tempo de reação é: ' + reactionTime + ' segundos';
-    logoNeec.style.display = 'none';
-    startButton.style.display = 'inline';
-}
+  printTime.innerHTML = "O teu tempo de reação é: " + reactionTime + " segundos";
+  logoNeec.style.display = "none";
+  startButton.style.display = "inline";
+};
 
-startButton.onclick = function() {
-    this.style.display = 'none';
-    printTime.innerHTML = '';
-    
-    gameStart = true;
-    startTime = 0;
-    clickedTime = 0;
-    reactionTime = 0;
+startButton.onclick = function () {
+  this.style.display = "none";
+  printTime.innerHTML = "";
 
-    
-    var t = 3;
-    var timer = setInterval(() => {
-        countdown.innerHTML = t--;
-        if (t < 0) {
-            countdown.innerHTML = '';
-            makeBox();
-            clearInterval(timer);
-        }
-    }, 1000);
-}
+  gameStart = true;
+  startTime = 0;
+  clickedTime = 0;
+  reactionTime = 0;
+  total_clicks = 0;
+  total_clicks = 0;
+  total.innerHTML = `Clicks fora do tempo: ${total_clicks}`;
+
+  var t = 3;
+  var timer = setInterval(() => {
+    countdown.innerHTML = t--;
+    if (t < 0) {
+      countdown.innerHTML = "";
+      makeBox();
+      clearInterval(timer);
+    }
+  }, 1000);
+};
